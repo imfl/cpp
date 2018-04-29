40d0dcfad6de3221800065097ffc7cad57902d2d
FL <lei.fu@connect.ust.hk>
2018-04-29 16:21:58 +0800

    ok

--- commands/dcal.cpp
+++ commands/dcal.cpp
@@ -9,11 +9,11 @@
 
 // For <ctime>, know:
 // 1. types:
-//  - time_t            : time since epoch
-//  - tm                    : time structure
+//  - time_t        : time since epoch
+//  - tm            : time structure
 // 2. functions:
-//  - time()            : time_t * => time_t (save in time_t *)
-//  - ctime()           : time_t * => char * ~ asctime(localtime(time_t *))
+//  - time()        : time_t * => time_t (save in time_t *)
+//  - ctime()       : time_t * => char * ~ asctime(localtime(time_t *))
 //  - asctime()     : tm * => char *
 //  - localtime()   : time_t * => tm *
 //  - gmtime()      : time_t * => tm * (Greenwich Mean Time)
@@ -21,12 +21,12 @@
 //  - strftime()    : (char *, size_t, const char *, const tm *) => size_t
 //  - difftime()    : (time_t, time_t) => double
 
-#include <cmath>            // std::abs
+#include <cmath>        // std::abs
 #include <cstdlib>      // std::atoi
 #include <ctime>
 #include <iostream>     // std::cin, std::cout, std::endl
 #include <sstream>      // std::ostringstream
-#include <string>           // std::string
+#include <string>       // std::string
 
 #define MAX 100
 #define OFFSET 9
@@ -36,7 +36,7 @@ void calc(int n)
 {
     std::ostringstream oss;
     oss << std::abs(n) << " day" << (std::abs(n) > 1 ? "s" : "")
-            << (n < 0 ? " before" : " after") << " today is ";
+        << (n < 0 ? " before" : " after") << " today is ";
     auto len = oss.str().size();
 
     std::time_t t = std::time(NULL);
@@ -66,10 +66,8 @@ int main(int argc, char * argv[])
         std::string enter = "Enter number of days before or after today"
                             " (q to quit): ";
         std::cout << enter;
-        for (int n = 0;
-                std::cin >> n;
-                std::cin.ignore(MAX, '\n'),
-                std::cout << '\n' << enter)
+        for (int n = 0; std::cin >> n;
+             std::cin.ignore(MAX, '\n'), std::cout << '\n' << enter)
             calc(n);
     }
     else
